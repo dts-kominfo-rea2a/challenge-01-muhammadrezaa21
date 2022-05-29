0c5dd4b2665e9d30b952be1028489a610da8e581
muhammadrezaa21 <mreza21031998@gmail.com>
2022-05-29 20:09:34 +0700

    origin

--- index.js
+++ index.js
@@ -4,17 +4,78 @@
 const firstUser = {};
 const secondUser = {};
 
+// 1. first user
+favoriteColorFirstUser = new Set(["Yellow", "Pink", "White", "Purple"]);
+educationFirstUser = [
+  { name: "SD 01", city: "Jakarta", graduate: 2016 },
+  { name: "SMP 02", city: "Jakarta", graduate: 2019 },
+  { name: "SMA 03", city: "Tangerang" },
+];
+favoriteRestaurantFirstUser = new Set([
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Bento",
+  "Eggy",
+  "Padang",
+  "Tteok",
+  "Sushi",
+  "Sushi",
+]);
+
+firstUser.name = "Monica";
+firstUser.gender = "Female";
+firstUser.age = 17;
+firstUser.email = "monica@dingdong.com";
+firstUser.favoriteColor = favoriteColorFirstUser;
+firstUser.isHavePet = true;
+firstUser.education = educationFirstUser;
+firstUser.favoriteRestaurant = favoriteRestaurantFirstUser;
+
+// 2. second user
+favoriteColorSecondUser = new Set(["Blue", "Black", "Grey"]);
+educationSecondUser = [
+  { name: "SD 02", city: "Jakarta", graduate: 2010 },
+  { name: "SMP 03", city: "Bogor", graduate: 2013 },
+  { name: "SMA 01", city: "Surabaya", graduate: 2016 },
+  { name: "Universitas Maju", city: "Tangerang" },
+];
+favoriteRestaurantSecondUser = new Set([
+  "Tempura",
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Padang",
+  "Katsu",
+  "Geprek",
+  "Pancake",
+  "Eggy",
+]);
+
+secondUser.name = "Wendy";
+secondUser.gender = "Male";
+secondUser.age = 23;
+secondUser.email = "wendy@dingdong.com";
+secondUser.favoriteColor = favoriteColorSecondUser;
+secondUser.isHavePet = false;
+secondUser.education = educationSecondUser;
+secondUser.favoriteRestaurant = favoriteRestaurantSecondUser;
+
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
